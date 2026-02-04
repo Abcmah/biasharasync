874f7ae33761d506f2f1b1e422a0fd3585534a2c
Bismarque <bismarquemomanyi@gmail.com>
2026-02-04 23:22:01 +0300

    company

--- app/SuperAdmin/Http/Controllers/Api/CompanyController.php
+++ app/SuperAdmin/Http/Controllers/Api/CompanyController.php
@@ -69,14 +69,14 @@ class CompanyController extends ApiBaseController
 
     public function updating(Company $company)
     {
-        if (env('APP_ENV') == 'production' && $company->email == 'company@example.com' && ($company->isDirty('name') ||
-            $company->isDirty('short_name') || $company->isDirty('light_logo') ||
-            $company->isDirty('dark_logo') || $company->isDirty('small_dark_logo') ||
-            $company->isDirty('small_light_logo') || $company->isDirty('app_debug') ||
-            $company->isDirty('update_app_notification') || $company->isDirty('app_debug')
-        )) {
-            throw new ApiException('Not Allowed In Demo Mode');
-        }
+        // if (env('APP_ENV') == 'production' && $company->email == 'company@example.com' && ($company->isDirty('name') ||
+        //     $company->isDirty('short_name') || $company->isDirty('light_logo') ||
+        //     $company->isDirty('dark_logo') || $company->isDirty('small_dark_logo') ||
+        //     $company->isDirty('small_light_logo') || $company->isDirty('app_debug') ||
+        //     $company->isDirty('update_app_notification') || $company->isDirty('app_debug')
+        // )) {
+        //     throw new ApiException('Not Allowed In Demo Mode');
+        // }
 
         $request = request();
 
